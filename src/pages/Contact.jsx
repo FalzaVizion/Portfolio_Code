cba09ff561a88e47f2d952d82c2fe11aac8af2a7
FalzaVizion <knosebastian2@gmail.com>
2025-10-14 11:39:51 -0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,9 +31,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
-          from_name: form.name,
+          name: form.name,
           to_name: "Sebastian",
-          from_email: form.email,
+          email: form.email,
           to_email: "knosebastian2@gmail.com",
           message: form.message,
         },
